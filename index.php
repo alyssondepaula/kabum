791dc2614bdbd873db677e297f503b3d1ca185bc
alysson <alyssonjuniordepaula@gmail.com>
2022-12-20 19:01:26 -0300

    home

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php 
-
+  echo "<style>";
+  include 'index.css';
+  echo "<style>";
 ?>
+</style>
 
 <!DOCTYPE html>
 <html>
-<head>
-<title>Site Oficial - Gerenciador</title>
-</head>
-    <body>
-    
-     <h1>HELLO WORLD</h1>
-
-    </body>
+    <head>
+         <title>Site Oficial - Gerenciador</title>
+    </head>
+<body>
+<header>
+<div class="upheader">
+  <div>
+    <button>Entrar</button>
+     <span>/</span>
+    <button>Cadastrar</button>
+  </div>
+</div>
+<div class="bottomheader"></div>
+</header>
+  <main>
+    <div class="content">
+        <h1 class="calltitle">Vem pro Gerenciador Kabum</h1>
+        <buton class="call">Cadastre-se agora</buton>
+    </div>
+  </main>
+  </body>
+<footer></footer>
 </html>
