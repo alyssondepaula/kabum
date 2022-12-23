542ed1894b67f2226af4f3e9c0502cfa63cb7538
alysson <alyssonjuniordepaula@gmail.com>
2022-12-22 23:48:48 -0300

    guard

--- src/Routes/MyRoutes.php
+++ src/Routes/MyRoutes.php
@@ -4,56 +4,87 @@ use Src\Share\Session;
 
 abstract class MyRoutes
 {
+
+    private function authGuard()
+    {
+        $isAuth = Session::isAuth();
+        if(!$isAuth){
+            header('location: /');
+            exit;
+        }
+
+        return;
+    }
+
+    private function isLoggedGuard()
+    {
+        $isAuth = Session::isAuth();
+        if($isAuth){
+            header('location: /app');
+            exit;
+        }
+
+        return;
+    }
     protected function home()
     {
+        $this->isLoggedGuard();
         include PAGES.'/Home/index.php';
         exit;
     }
 
     protected function signin()
     {
+        $this->isLoggedGuard();
         include PAGES.'/SignIn/index.php';
         exit;
     }
 
     protected function signup()
     {
+        $this->isLoggedGuard();
         include PAGES.'/SignUp/index.php';
         exit;
     }
 
     protected function app()
     {
+        $this->authGuard();
         include PAGES.'/App/index.php';
         exit;
     }
 
     protected function createuser()
     {
+        $this->authGuard();
         include PAGES.'/CreateClient/index.php';
         exit;
     }
 
     protected function editclient()
     {
+        $this->authGuard();
         include PAGES.'/EditClient/index.php';
         exit;
     }
 
     protected function addresses()
     {
+        $this->authGuard();
         include PAGES.'/Addresses/index.php';
         exit;
     }
 
     protected function createaddress()
     {
+        $this->authGuard();
         include PAGES.'/CreateAddress/index.php';
         exit;
     }
 
     protected function editaddress()
     {
+        $this->authGuard();
         include PAGES.'/EditAddress/index.php';
         exit;
     }
